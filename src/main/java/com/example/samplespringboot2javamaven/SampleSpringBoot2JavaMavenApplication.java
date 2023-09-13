deb241a9421905e7c20824dc4a1e9a6241db863c
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-09-13 18:37:37 +0900

    feat: 프로젝트 초기 구성

---
+++ src/main/java/com/example/samplespringboot2javamaven/SampleSpringBoot2JavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot2javamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot2JavaMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBoot2JavaMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplespringboot2javamaven/SampleSpringBoot2JavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot2javamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot2JavaMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
